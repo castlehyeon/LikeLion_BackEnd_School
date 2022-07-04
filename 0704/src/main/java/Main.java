b90278d616f07cf4e607a4faab4d206c652b941a
castlehyeon <qnftktls118@naver.com>
2022-07-04 14:42:32 +0900

    챕터13 3번

--- 0704/src/main/java/Main.java
+++ 0704/src/main/java/Main.java
@@ -126,59 +126,120 @@
 //                System.out.println(사용자명 + "가 활로 공격합니다.");
 //            }
 //        }
+//class Main {
+//    public static void main(String[] args) {
+//        전사 a전사 = new 전사();
+//        String 이름 = "칸";
+//        a전사.이름 = 이름;
+//        a전사.나이 = 20;
+//        a전사.자기소개();
+//        a전사.나이++;
+//        a전사.자기소개();
+//        a전사.나이 = 30;
+//        a전사.이름 = "카니";
+//        a전사.자기소개();
+//        a전사.a무기 = new 활();
+//        a전사.공격();
+//        // 출력 : 카니가 활로 공격합니다.
+//        a전사.a무기 = new 칼();
+//        a전사.공격();
+//        // 출력 : 카니가 칼로 공격합니다.
+//    }
+//}
+//
+//class 전사 {
+//    // 인스턴스 변수
+//    String 이름;
+//    // 인스턴스 변수
+//    int 나이;
+//    // 인스턴스 변수
+//    무기 a무기;
+//    void 자기소개() {
+//        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+//    }
+//    void 공격() {
+//        a무기.사용자명 = 이름;
+//        a무기.작동();
+//    }
+//}
+//
+//abstract class 무기 {
+//    String 사용자명;
+//    abstract void 작동(); //객체를 만들기 위한 클래스가 아님. "이 클래스는 리모콘 전용이니 쓰지마세요." 명시
+//}
+//
+//class 칼 extends 무기 {
+//    void 작동(){
+//        System.out.println(사용자명 + "가 칼로 공격합니다.");
+//    }
+//}
+//
+//class 활 extends 무기 {
+//    void 작동(){
+//        System.out.println(사용자명 + "가 활로 공격합니다.");
+//    }
+//}
+////new 칼();
+////new 활();
+////new 무기(); X
+// 힌트 : if ( a무기 == null ) { ... }
+
+//
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
-        String 이름 = "칸";
-        a전사.이름 = 이름;
-        a전사.나이 = 20;
-        a전사.자기소개();
-        a전사.나이++;
-        a전사.자기소개();
-        a전사.나이 = 30;
-        a전사.이름 = "카니";
-        a전사.자기소개();
-        a전사.a무기 = new 활();
-        a전사.공격();
-        // 출력 : 카니가 활로 공격합니다.
-        a전사.a무기 = new 칼();
-        a전사.공격();
-        // 출력 : 카니가 칼로 공격합니다.
-    }
-}
+        전사 a전사1 = new 전사();
 
-class 전사 {
-    // 인스턴스 변수
-    String 이름;
-    // 인스턴스 변수
-    int 나이;
-    // 인스턴스 변수
-    무기 a무기;
-    void 자기소개() {
-        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
-    }
-    void 공격() {
-        a무기.사용자명 = 이름;
-        a무기.작동();
-    }
-}
+        a전사1.a왼손무기 = new 칼();
+        a전사1.공격();
+        // 출력 => 전사가 왼손으로 칼(을)를 사용합니다.
 
-abstract class 무기 {
-    String 사용자명;
-    abstract void 작동(); //객체를 만들기 위한 클래스가 아님. "이 클래스는 리모콘 전용이니 쓰지마세요." 명시
-}
-
-class 칼 extends 무기 {
-    void 작동(){
-        System.out.println(사용자명 + "가 칼로 공격합니다.");
+        전사 a전사2 = new 전사();
+        a전사2.a왼손무기 = new 창();
+        a전사2.a오른손무기 = new 도끼();
+        a전사2.공격();
+        // 출력 => 전사가 왼손으로 창(을)를 사용합니다.
+        // 출력 => 전사가 오른손으로 도끼(을)를 사용합니다.
     }
 }
+class 전사{
+    무기 a왼손무기;
+    무기 a오른손무기;
+    void 공격(){
+        if(a왼손무기 != null){
+            System.out.print("전사가 왼손으로 ");
+            a왼손무기.작동();
+        }
+        if(a오른손무기 !=null){
+            System.out.print("전사가 오른손으로 ");
+            a오른손무기.작동();
+        }
+    }
+        }
+abstract class 무기{
+    abstract void 작동();
+}
+class 칼 extends 무기{
 
-class 활 extends 무기 {
-    void 작동(){
-        System.out.println(사용자명 + "가 활로 공격합니다.");
+    @Override
+    void 작동() {
+        System.out.println("칼(을)를 사용합니다.");
     }
 }
-//new 칼();
-//new 활();
-//new 무기(); X
+class 활 extends 무기{
+    @Override
+    void 작동() {
+        System.out.println("활(을)를 사용합니다.");
+    }
+}
+class 창 extends 무기{
+    @Override
+    void 작동() {
+        System.out.println("창(을)를 사용합니다.");
+    }
+}
+class 도끼 extends 무기{
+    @Override
+    void 작동() {
+        System.out.println("도끼(을)를 사용합니다.");
+    }
+}

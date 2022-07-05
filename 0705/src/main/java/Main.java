b9ed06c1831131eecb63a636371a07f239772207
castlehyeon <qnftktls118@naver.com>
2022-07-05 15:29:41 +0900

    챕터14 6번 문제

--- 0705/src/main/java/Main.java
+++ 0705/src/main/java/Main.java
@@ -57,25 +57,52 @@
 //}
 // 출력 : 아래와 같이 출력되게 해주세요.
 
+//class Main {
+//    public static void main(String[] args) {
+//        new 사람().걷다();
+//
+//        System.out.println(new 사람().a왼팔.길이 + "cm");
+//        // 출력 : 100cm
+//    }
+//}
+//class 사람 {
+//    팔 a왼팔 = new 팔(); //생성자를 안넣었기 때문에 NullPoint에러 발생
+//
+//    public 사람() {
+//        this.a왼팔.길이 = 100;
+//    }
+//
+//    void 걷다() {
+//        System.out.println("사람 걷는다.");
+//    }
+//}
+//class 팔{
+//    int 길이;
+//}
+// 문제 : 아래와 같이 출력 되도록 해주세요.
+
 class Main {
     public static void main(String[] args) {
-        new 사람().걷다();
+        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
 
-        System.out.println(new 사람().a왼팔.길이 + "cm");
-        // 출력 : 100cm
+        System.out.println(엄지손가락_길이 + "cm");
+        // 출력 : 5cm
     }
 }
-class 사람 {
-    팔 a왼팔 = new 팔(); //생성자를 안넣었기 때문에 NullPoint에러 발생
-
-    public 사람() {
-        this.a왼팔.길이 = 100;
-    }
-
-    void 걷다() {
-        System.out.println("사람 걷는다.");
-    }
+class 사람{
+    팔 a왼팔 = new 팔();
 }
 class 팔{
+//    int a손;
+    손 a손 = new 손();
+}
+class 손{
+    길이 a엄지손가락 = new 길이();
+}
+class 길이{
     int 길이;
+
+    public 길이() {
+        this.길이 = 5;
+    }
 }

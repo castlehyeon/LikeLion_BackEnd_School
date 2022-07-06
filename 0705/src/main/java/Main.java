31825e75acfc23c0f9079734a9df340fe84cf64a
castlehyeon <qnftktls118@naver.com>
2022-07-06 12:59:52 +0900

    0706 구성실습

--- 0705/src/main/java/Main.java
+++ 0705/src/main/java/Main.java
@@ -81,28 +81,39 @@
 //}
 // 문제 : 아래와 같이 출력 되도록 해주세요.
 
+//class Main {
+//    public static void main(String[] args) {
+//        int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
+//
+//        System.out.println(엄지손가락_길이 + "cm");
+//        // 출력 : 5cm
+//    }
+//}
+//class 사람{
+//    팔 a왼팔 = new 팔();
+//}
+//class 팔{
+////    int a손;
+//    손 a손 = new 손();
+//}
+//class 손{
+//    길이 a엄지손가락 = new 길이();
+//}
+//class 길이{
+//    int 길이;
+//
+//    public 길이() {
+//        this.길이 = 5;
+//    }
+//}
+// 문제 : 아래와 같이 출력 되도록 해주세요.
+// 조건 : 생성자를 오직 3개만 사용해서 만들어주세요.
+
 class Main {
     public static void main(String[] args) {
         int 엄지손가락_길이 = new 사람().a왼팔.a손.a엄지손가락.길이;
 
         System.out.println(엄지손가락_길이 + "cm");
         // 출력 : 5cm
-    }
-}
-class 사람{
-    팔 a왼팔 = new 팔();
-}
-class 팔{
-//    int a손;
-    손 a손 = new 손();
-}
-class 손{
-    길이 a엄지손가락 = new 길이();
-}
-class 길이{
-    int 길이;
-
-    public 길이() {
-        this.길이 = 5;
     }
 }

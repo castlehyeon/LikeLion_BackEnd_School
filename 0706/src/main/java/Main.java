6f53b671ef9ec2097e4a490d9be4d4188943ebd2
castlehyeon <qnftktls118@naver.com>
2022-07-07 01:41:54 +0900

    0706문제4번

--- 0706/src/main/java/Main.java
+++ 0706/src/main/java/Main.java
@@ -142,75 +142,57 @@
 // 단 : 전사는 무기라는 것이 존재한다는 것은 알지만 칼, 창, 지팡이에 대해선 몰라야 합니다.
 
 //내가 작성한 코드
+//
+
+// 문제 : 구성(전사는 무기로 구성된다.)을 사용하여 중복을 제거해주세요.
+// Main 클래스는 수정불가능
+
+
+//전사타입을 나눔으로써,공격메서드가 반복됨.
+//전사 클래스에서 무기멤버와 공격메서드 선언, 생성자만 사용
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        전사타입A a전사타입A = new 전사타입A();
+        a전사타입A.공격();
 
-        a전사.공격();
-        a전사.무기변경(1);
-        a전사.공격();
-        a전사.무기변경(2);
-        a전사.공격();
-        a전사.무기변경(3);
-        a전사.공격();
-      /*
-    칼(으)로 공격합니다.
-    데미지 : 78
-    창(으)로 공격합니다.
-    데미지 : 80
-    지팡이(으)로 공격합니다.
-    데미지 : 12
-    전설의_지팡이(으)로 공격합니다.
-    데미지 : 120
-    */
+        전사타입B a전사타입B = new 전사타입B();
+        a전사타입B.공격();
+
+        전사타입C a전사타입C = new 전사타입C();
+        a전사타입C.공격();
+
+        전사타입D a전사타입D = new 전사타입D();
+        a전사타입D.공격();
     }
 }
 
+abstract class 전사{
+    String a무기;
 
-class 전사 {
-    무기 a무기 = new 무기(); //다른 객체(무기)를 참조하는 참조변수!, 여기서 객체까지 생성하면 되겠군!
-    void 공격(){
-        a무기.사용();
-    }
-    void 무기변경(int a){
-        if(a == 1)
-            a무기 = new 창();
-        if(a == 2)
-            a무기 = new 지팡이();
-        if(a == 3)
-            a무기 = new 전설의_지팡이();
-    }
-}
-class 무기{
-    String 장비;
-    int 데미지;
-
-    public 무기() {
-        this.장비 = "칼";
-        this.데미지 = 78;
+    public 전사() {
+        this.a무기 = a무기;
     }
 
-    void 사용(){
-        System.out.println(장비+"(으)로 공격합니다.");
-        System.out.println("데미지 : "+데미지);
-    }
+    void 공격(){System.out.println(a무기+"로 공격");};
 }
 
-class 창 extends 무기 {
-    public 창() {
-        this.장비 = "창";
-        this.데미지 = 80;
+class 전사타입A extends 전사 {
+    public 전사타입A() {
+        this.a무기 = "칼";
     }
 }
-class 지팡이 extends 무기 {
-    public 지팡이() {
-        this.장비 = "지팡이";
-        this.데미지 = 12;
+class 전사타입B extends 전사 {
+    public 전사타입B() {
+        this.a무기 = "칼";
     }
 }
-class 전설의_지팡이 extends 무기 {
-    public 전설의_지팡이() {
-        this.장비 = "전설의 지팡이";
-        this.데미지 = 120;
+class 전사타입C extends 전사 {
+    public 전사타입C() {
+        this.a무기 = "활";
+    }
+}
+class 전사타입D extends 전사 {
+    public 전사타입D() {
+        this.a무기 = "활";
     }
 }

1e239e7e692f88279ea8773d4fb02adb2ba66d11
castlehyeon <qnftktls118@naver.com>
2022-07-14 11:21:57 +0900

    5번문제

--- project0714/src/main/java/Main.java
+++ project0714/src/main/java/Main.java
@@ -1,21 +1,29 @@
-//code.oa.gg/java8/995
+//code.oa.gg/java8/997
 
 // 문제 : 아래 코드가 작동하도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
         ArrayList ar = new ArrayList();
+        ar.add(100);
+        ar.add(200);
+        ar.add(300);
+        ar.add(400);
 
-        for ( int i = 0; i < 100; i++ ) {
-            ar.add((i + 1) * 10);
-        }
+        ar.remove(2);
+        int value = ar.get(2);
+        System.out.println(value);
+        // 출력 : 400
 
-        int ar_size = ar.size(); // ar_size의 값은 100 이어야 합니다.
+        ar.remove(0);
+        value = ar.get(0);
+        System.out.println(value);
+        // 출력 : 200
 
-        for ( int i = 0; i < ar_size; i++ ) {
-            int value = ar.get(i);
-            System.out.println(value);
-        }
+        ar.add(78);
+        value = ar.get(2);
+        System.out.println(value);
+        // 출력 : 78
     }
 }
 
@@ -53,7 +61,14 @@ class ArrayList {
         return datas[index];
     }
 
-    public int size() {
-        return lastIndex+1;
+    int size() {
+        return lastIndex + 1;
+    }
+
+    void remove(int index) {
+        for(int i = index; i < lastIndex; i++){ //삭제된 인덱스의 자리에 바로뒤에있는 요소들이 대체되는 for문
+            datas[index] = datas[index+1];
+        }
+        lastIndex -= 1; //최신 인덱스를 -1로 해주기.
     }
 }

a399cebed2650d87d7079451850ad5bbf3e6bad1
castlehyeon <qnftktls118@naver.com>
2022-07-14 11:09:46 +0900

    4번문제

--- project0714/src/main/java/Main.java
+++ project0714/src/main/java/Main.java
@@ -1,41 +1,59 @@
-//code.oa.gg/java8/991
+//code.oa.gg/java8/995
 
-// 문제 : 배열의 단점을 보완한 ArrayList 라는 클래스를 만들어주세요. 아래와 같이 출력되도록 해주세요.
+// 문제 : 아래 코드가 작동하도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
         ArrayList ar = new ArrayList();
-        ar.add(100);
-        ar.add(200);
-        ar.add(300);
 
-        int value = ar.get(0);
-        System.out.println(value);
-        // 출력 : 100
+        for ( int i = 0; i < 100; i++ ) {
+            ar.add((i + 1) * 10);
+        }
 
-        value = ar.get(1);
-        System.out.println(value);
-        // 출력 : 200
+        int ar_size = ar.size(); // ar_size의 값은 100 이어야 합니다.
 
-        value = ar.get(2);
-        System.out.println(value);
-        // 출력 : 300
+        for ( int i = 0; i < ar_size; i++ ) {
+            int value = ar.get(i);
+            System.out.println(value);
+        }
     }
 }
 
 class ArrayList {
-    //int[] arr = new int[3]; 내 코드
-    //강사님 코드: 생성자에서 해결하라.
-    int[] arr;
-    ArrayList(){
-        arr = new int[3];
+    int[] datas;
+    int lastIndex = -1;
+
+    ArrayList() {
+        datas = new int[3]; // 이 부분은 수정할 수 없습니다.
     }
-    int index =0;
-    public void add(int num){
-        arr[index] = num;
-        ++index;
+
+    void add(int data) {
+        if ( lastIndex + 1 >= datas.length ) {
+            // 확장공사
+            // 기존버스 버리고 새 버스로 연결!!
+            // datas 이 녀석이 기존 버스를 버리고 새 버스를 가리켜야 합니다.
+
+            // 새 버스 생성
+            int[] newArr = new int[datas.length * 2];
+
+            // 기존 버스(배열)를 버리기 전에 버스에 있던 승객들을 새 버스로 옮긴다.
+            for ( int i = 0; i < datas.length; i++ ) {
+                newArr[i] = datas[i];
+            }
+
+            datas = newArr;
+        }
+
+        lastIndex++;
+
+        datas[lastIndex] = data;
     }
-    public int get(int i){
-        return arr[i];
+
+    int get(int index) {
+        return datas[index];
+    }
+
+    public int size() {
+        return lastIndex+1;
     }
 }

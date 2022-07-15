e078844f29acf7c0271b92b7f2bab723615a6eda
castlehyeon <qnftktls118@naver.com>
2022-07-15 10:33:39 +0900

    TDD하지않고 오류

--- project0714/src/main/java/Main.java
+++ project0714/src/main/java/Main.java
@@ -1,29 +1,32 @@
-//code.oa.gg/java8/997
+// code.oa.gg/java8/1340
 
 // 문제 : 아래 코드가 작동하도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
         ArrayList ar = new ArrayList();
-        ar.add(100);
-        ar.add(200);
-        ar.add(300);
-        ar.add(400);
+        ar.add(100, 0);
+        ar.add(200, 1);
+        ar.add(300, 2);
+        ar.add(400, 3);
+        ar.add(500, 4);
+        ar.add(600, 2); // 2번좌석으로 새치기, 기존의 2번좌석 손님부터 끝 손님까지 뒤로 한칸씩 밀린다.
+        ar.add(700, 0); // 0번좌석으로 새치기, 기존의 0번좌석 손님부터 끝 손님까지 뒤로 한칸씩 밀린다.
 
-        ar.remove(2);
-        int value = ar.get(2);
-        System.out.println(value);
-        // 출력 : 400
+        for ( int i = 0; i < ar.size(); i++ ) {
+            int value = ar.get(i);
 
-        ar.remove(0);
-        value = ar.get(0);
-        System.out.println(value);
-        // 출력 : 200
+            System.out.println(i + " : " + value);
+        }
 
-        ar.add(78);
-        value = ar.get(2);
-        System.out.println(value);
-        // 출력 : 78
+        // 출력
+        // 0 : 700
+        // 1 : 100
+        // 2 : 200
+        // 3 : 600
+        // 4 : 300
+        // 5 : 400
+        // 6 : 500
     }
 }
 
@@ -35,7 +38,9 @@ class ArrayList {
         datas = new int[3]; // 이 부분은 수정할 수 없습니다.
     }
 
-    void add(int data) {
+    void add(int data, int index) {
+
+
         if ( lastIndex + 1 >= datas.length ) {
             // 확장공사
             // 기존버스 버리고 새 버스로 연결!!
@@ -55,8 +60,13 @@ class ArrayList {
         lastIndex++;
 
         datas[lastIndex] = data;
-    }
+        }
 
+    void addAt(int data, int index){
+        for ( int j = index; j < lastIndex; j++ ){
+
+        }
+    }
     int get(int index) {
         return datas[index];
     }
@@ -64,11 +74,7 @@ class ArrayList {
     int size() {
         return lastIndex + 1;
     }
-
-    void remove(int index) {
-        for(int i = index; i < lastIndex; i++){ //삭제된 인덱스의 자리에 바로뒤에있는 요소들이 대체되는 for문
-            datas[index] = datas[index+1];
-        }
-        lastIndex -= 1; //최신 인덱스를 -1로 해주기.
-    }
 }
+
+//sizeupfull함수만들기
+//add함수 오버로딩해서 하나 더 만들기

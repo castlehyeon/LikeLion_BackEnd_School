1a4175adb465cffb4841afc1e0d77c6f283c4c9c
castlehyeon <qnftktls118@naver.com>
2022-07-18 11:51:08 +0900

    0718 HashMap 2번

--- project0718/src/main/java/Main.java
+++ project0718/src/main/java/Main.java
@@ -1,39 +1,63 @@
-import java.util.ArrayList;
-
+//code.oa.gg/java8/1127
 class Main {
     public static void main(String[] args) {
-        // 여기서 `저장소<>` 는 `저장소<Integer>` 가 생략된 표현이다.
-        저장소<Integer> a정수저장소 = new 저장소<>();
-        a정수저장소.set(10);
-        System.out.println(a정수저장소.get() * a정수저장소.get());
-        // 출력 => 100
+        HashMap aMap = new HashMap();
 
-        저장소<Boolean> a논리저장소 = new 저장소<>();
-        a논리저장소.set(false);
+        aMap.put(1, "안녕");//위 아래 매개변수가 다 다르네 ->이를 일일이 만들기보다 통합형 <T>를 쓰자.
+        aMap.put("A", false);
 
-        if ( a논리저장소.get() ) {
-            System.out.println("참 : " + a논리저장소.get());
-        }
-        else {
-            System.out.println("거짓 : " + a논리저장소.get());
-        }
+        System.out.println(aMap.get("A"));
+        // 출력 : false
     }
 }
-class 저장소 <T> {
-    private Object data[];
-    public 저장소() {
-       // 저장소<T> 저장소 = new 저장소<>();
+class HashMap <T> {
+    private Object[] key;
+    private Object[] value;
+    private int lastIndex;
+    public HashMap() {
+        key = new Object[3];
+        value = new Object[key.length];//3말고 key.length로 쓰는게 좋다.
+        lastIndex = -1;
+    }
+    public void put(T key, T value){
+        sizeUpIfNeedKey();
+        sizeUpIfNeedValue();
+        ++lastIndex;
+        this.key[lastIndex] = key;
+        this.value[lastIndex] = value;
+
     }
 
-    public T getData() {
-        return (T) data;
+    private void sizeUpIfNeedValue() {
+        if(lastIndex >= key.length){
+            Object[] newValue = new Object[key.length * 2];
+            for ( int i = 0; i < key.length; i++ ) {
+                newValue[i] = value[i];
+            }
+            value = newValue;
+        }
     }
 
-    public void setData(Object[] data) {
-        this.data = data;
+    private void sizeUpIfNeedKey() {
+        if(lastIndex >= key.length){
+            Object[] newKey = new Object[key.length * 2];
+            for ( int i = 0; i < key.length; i++ ) {
+                newKey[i] = key[i];
+            }
+            key = newKey;
+        }
     }
-    public T set(int data){
 
-        return T
+    public T get(T input) {
+        //rs1에는 인덱스가 들어가야한다. 하지만 KeyIndex는 꼭 int가 들어가는 것은 아니다.
+        T rs1, rs2 = null;
+
+        for(int i = 0; i <= lastIndex; i++) {
+            //boolean result = key[i].equals(input);
+            if (key[i] == input) {
+                rs2 = (T) value[i];
+            }
+        }
+        return rs2;
     }
 }
